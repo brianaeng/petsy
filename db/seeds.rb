122ae66990ca39453fb47f10ca55b48d3f67f014
Erin Bond <erin@erinebond.com>
2016-10-18 14:20:02 -0700

    Seeded farm animal data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+
+require "csv"
+
+CSV.foreach('seeds_csvs/farm.csv', :headers => true) do |csv_obj|
+  Product.create( name: csv_obj['name'], user_id: csv_obj['user_id'], price: cvs_obj['price'], quantity: cvs_obj['quantity'], exotic: cvs_obj['exotic'], farm: csv_obj['farm'], domestic: csv_obj['domestic'],description: cvs_obj['description'],picture: csv_obj['picture'])
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
