b20e12e9297c81f1fca01beb2258f778c8ee06ce
Erin Bond <erin@erinebond.com>
2016-10-19 19:35:49 -0700

    Users can now edit their own name, email and username on their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,46 @@
 class UsersController < ApplicationController
   # before_action :find_user only: [:show, :edit, :update]
+  def user
+    @user ||= User.find(params[:id].to_i)
+  end
 
   def new
+    @user = User.new
   end
 
   def create
+
   end
 
-  def index
-  end
+  # def index
+  # end
 
   def show
-    find_user
+    user
     @products = Product.where(user_id: @user.id )
   end
 
   def edit
+    user
   end
 
   def update
+    user.update_attributes(user_params)
+    redirect_to user_path(user.id)
   end
 
   def destroy
   end
 
   private  # Limited to just this class
-
+    def user_params
+      params.require(:user).permit(:name, :email, :username)
+    end
   # def student_params  # This is for security --> it will pull everything for :student key and only permit people to use the keys :first_name and :last_name
   #   params.require(:user).permit(:name)
   # end
 
-  def find_user
-    @user = User.find(params[:id])
-  end
+  # def find_user
+  #   @user = User.find(params[:id])
+  # end
 end
