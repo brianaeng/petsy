c78594e6244aa889bdfbd606cbe86ff6ad6da43d
Briana Eng <brianaeng@gmail.com>
2016-10-19 22:22:24 -0700

    Products are saved as active and to the specific user (I think, need to test more). Creating a product now redirects to the show page for that new product. The product form now populates the categories BUT when making a product it's not saving selected categories to it.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
+  has_many :product_categories
   has_many :categories, through: :product_categories, foreign_key: :category_id
+  accepts_nested_attributes_for :product_categories, :allow_destroy => true, :reject_if => :all_blank
   has_many :orders, through: :order_products
   validates :name, presence: true
   #validates :user_id, presence: true
