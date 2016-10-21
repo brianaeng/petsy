c2a847593696fb5ee6a96be4f6efcaf2cf97d280
Erin Bond <erin@erinebond.com>
2016-10-21 11:58:06 -0700

    Average ratings needs to be tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  # before_action :find_user only: [:show, :edit, :update]
   def user
     @user ||= User.find(params[:id].to_i)
   end
@@ -41,20 +40,13 @@ class UsersController < ApplicationController
       ratings_for_seller_products << product.rating
     end
 
-    seller_average_rating = ratings_for_seller_products.reduce(:+)/ratings_for_seller_products
+    @seller_average_rating = ratings_for_seller_products.reduce(:+)/ratings_for_seller_products.length
 
-    return seller_average_rating
+    return @seller_average_rating
   end
-  
-  private  # Limited to just this class
+
+  private
     def user_params
       params.require(:user).permit(:name, :email, :username)
     end
-  # def student_params  # This is for security --> it will pull everything for :student key and only permit people to use the keys :first_name and :last_name
-  #   params.require(:user).permit(:name)
-  # end
-
-  # def find_user
-  #   @user = User.find(params[:id])
-  # end
 end
