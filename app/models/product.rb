11058e24f4aa810d8b4341d6ceb2ab8c8e958980
Briana Eng <brianaeng@gmail.com>
2016-10-21 14:47:21 -0700

    Uncommenting user validation...

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :order_products
 
   validates :name, presence: true
-  # validates :user_id, presence: true
+  validates :user_id, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :description, presence: true
