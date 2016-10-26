3a282d20f0685da21313bb4f6644e52a74badcbf
Erin Bond <erin@erinebond.com>
2016-10-25 20:01:55 -0700

    Debugging the cc-number

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,7 +36,6 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     modified_params = order_params
     puts modified_params[:cc_number]
-    binding.pry
     modified_params[:cc_number] = modified_params[:cc_number].to_s.split("").last(4).join
     puts modified_params[:cc_number]
     @order.update(modified_params)
