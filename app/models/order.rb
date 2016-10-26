e3ffa27a525c2ee2b93a578fa850e55445a37b74
Erin Bond <erin@erinebond.com>
2016-10-26 09:33:08 -0700

    Order model modified valdiations for credit card information to only apply when the status is applied

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,12 +6,22 @@ class Order < ActiveRecord::Base
 
   validates :buyer_id, presence: true
   validates_inclusion_of :status, :within => ["pending","paid","complete","cancelled"], :message => "{{value}} is not a valid status"
-  # validates :cc_number, length: { is: 4 }
-  # validate :not_expired
+  validates :cc_4_digits
+  validate :cc_not_expired
 
-  def not_expired
-    if self.cc_expiration != nil && self.cc_expiration < Date.today
-      errors.add(:cc_expiration, "That credit card is expired.")
+  def cc_not_expired
+    if self.status == "paid"
+      if self.cc_expiration != nil && self.cc_expiration < Date.today
+        errors.add(:cc_expiration, "That credit card is expired.")
+      end
+    end
+  end
+
+  def cc_4_digits
+    if self.status == "paid"
+      if self.cc_number != nil && self.cc_number.length != 4
+        errors.add(:cc_expiration, "Only store four.")
+      end
     end
   end
 
@@ -21,6 +31,13 @@ class Order < ActiveRecord::Base
       total += op.subtotal
     end
     return total
+  end
+
+  def cant_buy_from_self
+    self.products.each do |product|
+      if product.user_id == self.buyer_id
+        errors.add(:buyer_id, "You are trying to buy a product that you sell.")
+      end
   end
 
   #status can only be complete if all of the order-products associated with that order have been shipped
