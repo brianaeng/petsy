37db36cbb6017d0f9dd99f8aa3c92a631aa106f2
Briana Eng <brianaeng@gmail.com>
2016-10-26 09:54:05 -0700

    Added comments for future consideration

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ActiveRecord::Base
   has_many :products, through: :product_categories#, foreign_key: :product_id
   #accepts_nested_attributes_for :products, :allow_destroy => true, :reject_if => :all_blank
   validates :name, uniqueness: true
+  #DO NOT VALIDATE NAME PRESENCE TRUE!
 end
